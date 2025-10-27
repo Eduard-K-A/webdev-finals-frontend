b58e4dce61dd5afec4466890c3ec838eac8f79e9
Shan Platon <shan_christian_platon@dlsl.edu.ph>
2025-10-28 00:11:48 +0800

    style: change background color

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,13 +6,13 @@ import FeaturedHotels from '../components/Hotels/FeaturedHotel';
 
 const HomePage: React.FC = () => {
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col bg-[#F7F1ED]">
             <Header type="home" />
             
             <main className="flex-grow pt-20">
                 <FeaturedHotels />
                 
-                <section className="py-16 bg-gray-50 px-4 text-center">
+                <section className="py-16 bg-[#F7F1ED] px-4 text-center">
                     <h2 className="text-2xl font-semibold mb-3">Hotel Room Reservation System</h2>
                     <p className="text-gray-600 max-w-2xl mx-auto">
                       Book and reserve rooms at the best hotels worldwide with ease and convenience!

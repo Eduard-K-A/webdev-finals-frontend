2be100f590fb35a9cf114582ed1c3a0834775459
Shan Platon <shan_christian_platon@dlsl.edu.ph>
2025-10-28 00:13:34 +0800

    style: add animation in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,15 @@ module.exports = {
           "0%": { transform: "translateX(100%)", opacity: "0" },
           "100%": { transform: "translateX(0)", opacity: "1" },
         },
+        slideUpFadeIn: {
+          "0%": { transform: "translateY(50%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
       },
       animation: {
         wave: "wave 0.5s ease-in-out forwards",
         "slide-fade-in": "slideFadeIn 0.8s ease-out forwards",
+        "slide-up-fade-in": "slideUpFadeIn 0.8s ease-out forwards",
       },
     },
   },

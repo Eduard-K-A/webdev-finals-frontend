bae06175bf111ceb049b8d54dd8392ae38fbb0aa
Shan Platon <shan_christian_platon@dlsl.edu.ph>
2025-10-28 00:08:09 +0800

    style: add image for dashboard and change font colors

--- src/components/Dashboard/Header.tsx
+++ src/components/Dashboard/Header.tsx
@@ -11,9 +11,11 @@ const Header: React.FC<HeaderProps> = ({ type = "home" }) => {
     const isHomePage = type === "home";
     return (
         <header className={`
-            bg-gray-500 text-black 
+            bg-cover bg-center bg-no-repeat text-black 
             ${isHomePage ? 'pb-20' : 'pb-8 pt-4'} 
-        `}>
+            `}
+            style={{ backgroundImage: "url('/assets/dashboardImg.png')" }}
+        >
             <div className="max-w-7xl mx-auto px-4">
             
                 {isHomePage && (
@@ -23,11 +25,11 @@ const Header: React.FC<HeaderProps> = ({ type = "home" }) => {
                             text-5xl md:text-6xl font-extrabold mb-4 
                             tracking-tight leading-tight
                         ">
-                            Book your next <span className="text-yellow-400">unforgettable stay.</span>
+                            Book your next <span className="text-[#FFFFFF]">unforgettable stay.</span>
                         </h1>
                         <p className="
-                            text-lg md:text-xl font-light mb-8 
-                            text-gray-300 max-w-3xl mx-auto
+                            text-lg md:text-xl font-medium mb-8 
+                            text-[#FFFFFF] max-w-3xl mx-auto
                         ">
                             Reserve and explore the best hotels worldwide with ease and convenience!
                         </p>

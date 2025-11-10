4f2279d158674460eab7081fbef49e550a94cb78
Eduard <eduard_king_anterola@dlsl.edu.ph>
2025-11-10 11:23:21 +0800

    feat: import Book and MyBooking pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./components/Dashboard/HomePage";
 import { SearchContextProvider } from "./context/SearchContext";
-import Bookings from "./pages/Bookings";
+import MyBookings from "./pages/User/MyBooking";
+import Book from "./pages/User/Book";
+
 import SignIn from "./pages/Login/SignIn";
 import Register from "./pages/Login/Register";
 import NavBar from "./components/Dashboard/NavBar";
-import HotelDetail from "./pages/HotelDetail";
-import Terms from "./pages/TermsAndConditions";
-import Policy from "./pages/PrivacyPolicy";
-import ManageRoom from "./pages/ManageRoom";
-import Rooms from "./pages/Rooms";
+import RoomDetail from "./pages/Room/RoomDetail";
+import Terms from "./pages/Terms/TermsAndConditions";
+import Policy from "./pages/Terms/PrivacyPolicy";
+import ManageRoom from "./pages/Admin/ManageRoom";
+import RoomLayout from "./pages/Room/RoomLayout";
 
 function App() {
   return (
@@ -19,11 +21,13 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/Hotels/Search" element={<div>Hotel List Page</div>} />
-          <Route path="/Rooms" element={<Rooms />} />
-          <Route path="/MyBookings" element={<Bookings />} />
+          <Route path="/Rooms" element={<RoomLayout />} />
+          <Route path='/Rooms/Search' element={<RoomLayout />} />
+          <Route path="/Book/:roomId" element={<Book />} />
+          <Route path="/MyBookings" element={<MyBookings />} />
           <Route path="/Signin" element={<SignIn />} />
           <Route path="/Register" element={<Register />} />
-          <Route path="/Hotels/:id" element={<HotelDetail />} />
+          <Route path="/Hotels/:id" element={<RoomDetail />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/policy" element={<Policy />} />
           <Route path="/ManageRoom" element={<ManageRoom />} />

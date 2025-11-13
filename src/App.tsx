521d404d337c78082d85be82a11fd8ae68ded1e0
Eduard <eduard_king_anterola@dlsl.edu.ph>
2025-11-13 10:17:53 +0800

    feat: add auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/Dashboard/HomePage";
 import { SearchContextProvider } from "./context/SearchContext";
+import { AuthContextProvider } from "./context/AuthContext";
 import MyBookings from "./pages/User/MyBooking";
 import Book from "./pages/User/Book";
 
@@ -15,27 +16,29 @@ import RoomLayout from "./pages/Room/RoomLayout";
 
 function App() {
   return (
-    <SearchContextProvider>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/Hotels/Search" element={<div>Hotel List Page</div>} />
-          <Route path="/Rooms" element={<RoomLayout />} />
-          <Route path='/Rooms/Search' element={<RoomLayout />} />
-          <Route path="/Book/:roomId" element={<Book />} />
-          <Route path="/MyBookings" element={<MyBookings />} />
-          <Route path="/Signin" element={<SignIn />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/Hotels/:id" element={<RoomDetail />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/policy" element={<Policy />} />
-          <Route path="/ManageRoom" element={<ManageRoom />} />
+    <AuthContextProvider>
+      <SearchContextProvider>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/Hotels/Search" element={<div>Hotel List Page</div>} />
+            <Route path="/Rooms" element={<RoomLayout />} />
+            <Route path='/Rooms/Search' element={<RoomLayout />} />
+            <Route path="/Book/:roomId" element={<Book />} />
+            <Route path="/MyBookings" element={<MyBookings />} />
+            <Route path="/Signin" element={<SignIn />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/Hotels/:id" element={<RoomDetail />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/policy" element={<Policy />} />
+            <Route path="/ManageRoom" element={<ManageRoom />} />
 
-          <Route path="*" element={<div>404 Not Found</div>} />
-        </Routes>
-      </Router>
-    </SearchContextProvider>
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Routes>
+        </Router>
+      </SearchContextProvider>
+    </AuthContextProvider>
   );
 }
 

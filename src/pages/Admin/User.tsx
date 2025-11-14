4258a26f48ed9e71ffdc7fc00009e77c74c263ce
Eduard <eduard_king_anterola@dlsl.edu.ph>
2025-11-14 14:22:48 +0800

    feat: add user page for admin

---
+++ src/pages/Admin/User.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const User: React.FC = () => {
+  return (
+    <div>
+      this is user page
+      must list all admins, and Users
+      must have a way to be able to create another admin account (role is initialized to admin)
+    </div>
+  )
+}
+
+export default User
